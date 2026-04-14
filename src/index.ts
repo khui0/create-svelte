6fbe6899ec34c686cf9e590b30ede7853c923c18
Kenny <101839505+khui0@users.noreply.github.com>
2026-04-14 18:37:23 -0400

    fix: Vite config

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,13 @@ const prettierContent = stripIndent`
 
 const viteContent = stripIndent`
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     sveltekit(),
     Icons({
       compiler: "svelte",
